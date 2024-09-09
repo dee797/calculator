d4498f1e4a593a103b69c77f22d2fbd16bfc5f88
dee <178827694+dee797@users.noreply.github.com>
2024-09-09 17:48:22 -0600

    Create object to hold references to button nodes

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function divideNumbers(num1, num2) {
     return num1 / num2;
 }
 
-
 function operate(num1, operator, num2) {
     let result;
     switch (operator) {
@@ -28,4 +27,44 @@ function operate(num1, operator, num2) {
             result = divideNumbers(num1, num2);
     }
     return result;
+}
+
+
+function createEventListeners() {
+    const buttons = {
+
+        digits: {
+            zeroBtn: document.querySelector("#0"),
+            oneBtn: document.querySelector("#1"),
+            twoBtn: document.querySelector("#2"),
+            threeBtn: document.querySelector("#3"),
+            fourBtn: document.querySelector("#4"),
+            fiveBtn: document.querySelector("#5"),
+            sixBtn: document.querySelector("#6"),
+            sevenBtn: document.querySelector("#7"),
+            eightBtn: document.querySelector("#8"),
+            nineBtn: document.querySelector("#9"),
+        },
+
+        operators: {
+            divideBtn: document.querySelector("#divide"),
+            multiplyBtn: document.querySelector("#multiply"),
+            subtractBtn: document.querySelector("#subtract"),
+            addBtn: document.querySelector("#add"),
+        },
+
+        other: {
+            clearBtn: document.querySelector("#clear"),
+            equalsBtn: document.querySelector("#equals"),
+            decimalBtn: document.querySelector("#decimal")
+        }
+
+    }
+    
+}
+
+
+
+function main() {
+
 }
