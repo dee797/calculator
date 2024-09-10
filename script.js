3a19e01da08c121935637266173d6d1c9260ba58
dee <178827694+dee797@users.noreply.github.com>
2024-09-10 11:41:41 -0600

    Add logic to display inputs out to user
    When user clicks on a button (for now only digit
    buttons work), a mouse click event will trigger
    and call the function displayInputs(), which will
    display the digit on the page if the length of the
    display has not yet reached 15 characters

--- script.js
+++ script.js
@@ -34,45 +34,39 @@ function operate(num1, operator, num2) {
 
 function getButtons() {
     const buttons = {
-
-        digits: {
-            zeroBtn: document.querySelector("#0"),
-            oneBtn: document.querySelector("#1"),
-            twoBtn: document.querySelector("#2"),
-            threeBtn: document.querySelector("#3"),
-            fourBtn: document.querySelector("#4"),
-            fiveBtn: document.querySelector("#5"),
-            sixBtn: document.querySelector("#6"),
-            sevenBtn: document.querySelector("#7"),
-            eightBtn: document.querySelector("#8"),
-            nineBtn: document.querySelector("#9"),
-        },
-
-        operators: {
-            divideBtn: document.querySelector("#divide"),
-            multiplyBtn: document.querySelector("#multiply"),
-            subtractBtn: document.querySelector("#subtract"),
-            addBtn: document.querySelector("#add"),
-        },
-
-        other: {
-            clearBtn: document.querySelector("#clear"),
-            equalsBtn: document.querySelector("#equals"),
-            decimalBtn: document.querySelector("#decimal")
-        }
-
+        digits: document.querySelectorAll(".digit"),
+        operators: document.querySelectorAll(".operator"),
+        other: document.querySelectorAll(".other")
     }
 
     return buttons;
 }
 
 
+function displayInputs(input) {
+    const display = document.querySelector("body .display");
+    let displayContent = display.textContent;
+
+    if (displayContent.length < 15) {
+        displayContent += input;
+        display.textContent = displayContent;
+    }
+}
+
+
+
 function createEventListeners() {
     const buttons = getButtons();
 
-    for (const property of Object.entries(buttons.digits)) {
-        property.addEventListener("click", () => {
+    for (const button of buttons["digits"]) {
+        button.addEventListener("click", () => {
+            displayInputs(button.id);
+        });
+    }
 
+    for (const button of buttons["operators"]) {
+        button.addEventListener("click", () => {
+            displayInputs(button.value);
         });
     }
 }
@@ -80,8 +74,14 @@ function createEventListeners() {
 
 
 function main() {
+    getButtons();
+    createEventListeners();
+
     let firstNum;
     let operator;
     let secondNum;
 
 }
+
+
+main();
